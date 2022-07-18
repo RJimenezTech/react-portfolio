32276fae9df15f35aac162bcb1e9061534b2991e
Robert Jimenez <robertjimenez86@gmail.com>
2022-07-17 20:03:34 -0500

    added contact form

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,13 +1,85 @@
-import React from "react";
+import React, {useState} from "react";
+import {validateEmail} from "../../utils/helpers";
 
 function Contact() {
+  const [formState, setFormState] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const [errorMessage, setErrorMessage] = useState("");
+  const { name, email, message } = formState;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!errorMessage) {
+      setFormState({ [e.target.name]: e.target.value });
+      console.log("Form", formState);
+    }
+  };
+
+  const handleChange = (e) => {
+    if (e.target.name === "email") {
+      const isValid = validateEmail(e.target.value);
+      if (!isValid) {
+        setErrorMessage("Your email is invalid.");
+      } else {
+        setErrorMessage("");
+      }
+    } else {
+      if (!e.target.value.length) {
+        setErrorMessage(`${e.target.name} is required.`);
+      } else {
+        setErrorMessage("");
+      }
+    }
+  };
+  
   return (
     <section className="my-5" id="contact">
       <div className="my-2">
         <h2>Contact Me</h2>
+        <form id="contact-form" onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            name="name"
+            defaultValue={name}
+            onBlur={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email address:</label>
+          <input
+            type="email"
+            name="email"
+            defaultValue={email}
+            onBlur={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="message">Message:</label>
+          <textarea
+            name="message"
+            rows="5"
+            defaultValue={message}
+            onBlur={handleChange}
+          />
+        </div>
+        {errorMessage && (
+          <div>
+            <p className="error-text">{errorMessage}</p>
+          </div>
+        )}
+        <button data-testid="button" type="submit">
+          Submit
+        </button>
+      </form>
 
-        <p>Drop me an email at <a href="mailTo:rjimeneztech@gmail.com" target="_blank" rel="noopener noreferrer" className="contact-link">RJimenezTech@gmail.com</a>.</p>
         <p>Want to get to know me more?</p>
+        <p>Drop me an email at <a href="mailTo:rjimeneztech@gmail.com" target="_blank" rel="noopener noreferrer" className="contact-link">RJimenezTech@gmail.com</a>.</p>
         <p>Check out my GitHub: <a href="http://github.com/RJimenezTech" target="_blank" rel="noopener noreferrer" className="contact-link">github.com/RJimenezTech</a>.</p>
         <p>Check out my tech Twitter: <a href="https://twitter.com/rjimeneztech" target="_blank" rel="noopener noreferrer" className="contact-link">@RJimenezTech</a>.</p>
         <p>My language learning Twitter: <a href="https://twitter.com/rjimeneztech" target="_blank" rel="noopener noreferrer" className="contact-link">@language_future</a>.</p>
